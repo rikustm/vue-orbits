28e1de70f0e15f1c1af8b205bc35560574621807
Rikus Rus <rikus.rus@gmail.com>
2024-11-24 13:57:39 +0100

    velovity

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,11 +1,10 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import d3ForceMagnetic from "d3-force-magnetic";
 
-const nodes = ref([
-  { fx: 0, fy: 0 },
-  { x: 100, y: 0 },
-]);
+const paused = ref(false);
+const tick = ref(0);
+const velocityFactor = ref(1);
 
 const orbitDistance = 166.66666666;
 const initialV = 5.270462766947299;
@@ -25,35 +24,101 @@ const forceSim = d3
   .forceSimulation()
   .alphaDecay(0)
   .velocityDecay(0)
-  .stop()
   .force(
     "gravity",
     d3ForceMagnetic()
       .strength(G)
       .charge((d) => d.mass)
   )
+  .on("tick", () => {
+    tick.value++;
+  })
   .nodes([{ fx: 0, fx: 0, mass: centralMass }, satellite.value]);
 
 resetSatellite();
 
-forceSim.restart().on("tick", () => {});
+//forceSim.restart().on("tick", () => {});
 
-function onTicked() {
-  simulation.tick(10);
+function onPause() {
+  paused.value = !paused.value;
+  if (paused.value) {
+    forceSim.stop();
+  } else {
+    forceSim.restart();
+  }
 }
+
+function onForward() {
+  forceSim.tick(1);
+  tick.value++;
+}
+
+function onBack() {
+  tick.value = tick.value - 1;
+  resetSatellite();
+  forceSim.restart().tick(tick.value).stop();
+}
+
+function onVelocityChange(e) {
+  velocityFactor.value = e.target.value;
+  satellite.value.vx *= velocityFactor.value;
+  satellite.value.vy *= velocityFactor.value;
+}
+
+function onAcelerate(factor) {
+  velocityFactor.value *= factor;
+  satellite.value.vx *= factor;
+  satellite.value.vy *= factor;
+}
+
+function onReset() {
+  resetSatellite();
+  tick.value = 0;
+}
+
+const currentVelocity = computed(() => {
+  return Math.sqrt(
+    Math.pow(satellite.value.vx, 2) + Math.pow(satellite.value.vy, 2)
+  ).toFixed(2);
+});
 </script>
 
 <template>
-  <div><button class="btn btn-primary" @click="onTicked">Step</button></div>
+  <div class="p-2 space-x-2 flex items-center">
+    <button class="btn btn-primary" @click="onPause">Play/Pause</button>
+    <button class="btn btn-primary" @click="onBack"><</button>
+    <button class="btn btn-primary" @click="onForward">></button>
+    <button class="btn btn-primary" @click="onReset">Reset</button>
+    <button class="btn btn-primary" @click="onAcelerate(0.99)"><<</button>
+    <button class="btn btn-primary" @click="onAcelerate(1.01)">>></button>
+    <!-- <div class="inline-block">
+      <input
+        @change="onVelocityChange"
+        type="range"
+        min="0"
+        max="2"
+        value="1"
+        class="range range-primary"
+        step="0.25"
+      />
+    </div> -->
+  </div>
   <div>
     <svg width="600" height="600">
       <g transform="translate(300,300)">
-        <circle r="20"></circle>
-        <circle r="10" :cx="satellite.x" :cy="satellite.y"></circle>
+        <circle r="20" fill="gold"></circle>
+        <circle
+          r="10"
+          fill="green"
+          :cx="satellite.x"
+          :cy="satellite.y"
+        ></circle>
       </g>
     </svg>
   </div>
   <div>
+    <pre>{{ currentVelocity }}</pre>
+    <pre>{{ tick }}</pre>
     <pre>{{ satellite }}</pre>
   </div>
 </template>
