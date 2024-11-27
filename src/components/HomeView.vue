67ba55547fbcc1826f3245c319bd18984295a78c
Rikus Rus <rikus.rus@gmail.com>
2024-11-27 20:18:34 +0100

    nested datamodel

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,22 +3,68 @@ import { ref, computed } from "vue";
 import d3ForceMagnetic from "d3-force-magnetic";
 
 const paused = ref(false);
-const tick = ref(0);
-const velocityFactor = ref(1);
+const ticks = ref(0);
 
-const orbitDistance = 166.66666666;
-const initialV = 5.270462766947299;
-const G = 4629.629629629629;
-const centralMass = 1;
+//Get sun/planet/satillite data
+import solarSystem from "./../data/solarSystem.json";
 
-const satellite = ref({ mass: 0 });
-const resetSatellite = () => {
-  satellite.value.x = -orbitDistance;
-  satellite.value.y = 0;
-  satellite.value.vx = 0;
-  satellite.value.vy = initialV;
-};
-resetSatellite();
+//Set constants/scales
+const width = 800;
+const height = 800;
+const maxDistance = d3.max(
+  solarSystem.map((d) =>
+    d3.max(d.satellites.map((s) => d.distance + s.distance))
+  )
+);
+const au = d3
+  .scaleLinear() // Astronomical unit
+  .domain([0, maxDistance * 2.1])
+  .range([0, Math.min(width, height)]);
+const G = 0.0005;
+const pxG = G * Math.pow(au(1), 3);
+
+const nodes = ref(parseBodies(solarSystem));
+
+function parseBodies(
+  bodies,
+  parentMass = 0,
+  posOffset = [0, 0],
+  velocityOffset = [0, 0]
+) {
+  return [].concat(
+    ...bodies.map((body) => {
+      const ang =
+        ((body.phase !== undefined ? body.phase : Math.random() * 360) *
+          Math.PI) /
+        180; // Random init angle if not specified (to prevent aligned init forces from distorting orbits)
+
+      const x = posOffset[0] + au(body.distance) * Math.sin(ang);
+      const y = posOffset[1] - au(body.distance) * Math.cos(ang);
+
+      const relVelocity =
+        (body.distance
+          ? Math.sqrt((pxG * parentMass) / au(body.distance))
+          : 0) * (body.factorV || 1); // orbital velocity: sqrt(GM/d)
+
+      const vx = velocityOffset[0] + relVelocity * Math.cos(ang);
+      const vy = velocityOffset[1] + relVelocity * Math.sin(ang);
+
+      return [
+        {
+          name: body.name,
+          color: body.color,
+          r: au(body.r),
+          mass: body.mass,
+          x,
+          y,
+          vx,
+          vy,
+        },
+        ...parseBodies(body.satellites || [], body.mass, [x, y], [vx, vy]),
+      ];
+    })
+  );
+}
 
 const forceSim = d3
   .forceSimulation()
@@ -27,17 +73,15 @@ const forceSim = d3
   .force(
     "gravity",
     d3ForceMagnetic()
-      .strength(G)
-      .charge((d) => d.mass)
-  )
-  .on("tick", () => {
-    tick.value++;
-  })
-  .nodes([{ fx: 0, fx: 0, mass: centralMass }, satellite.value]);
+      .id((d) => d.id)
+      .charge((node) => node.mass)
+  );
 
-resetSatellite();
+forceSim.nodes(nodes.value).force("gravity").strength(pxG);
 
-//forceSim.restart().on("tick", () => {});
+forceSim.on("tick", () => {
+  ticks.value++;
+});
 
 function onPause() {
   paused.value = !paused.value;
@@ -49,76 +93,60 @@ function onPause() {
 }
 
 function onForward() {
-  forceSim.tick(1);
-  tick.value++;
+  // forceSim.tick(1);
+  // tick.value++;
 }
 
 function onBack() {
-  tick.value = tick.value - 1;
-  resetSatellite();
-  forceSim.restart().tick(tick.value).stop();
+  // tick.value = tick.value - 1;
+  // resetSatellite();
+  // forceSim.restart().tick(tick.value).stop();
 }
 
-function onVelocityChange(e) {
-  velocityFactor.value = e.target.value;
-  satellite.value.vx *= velocityFactor.value;
-  satellite.value.vy *= velocityFactor.value;
-}
+function onVelocityChange(e) {}
 
-function onAcelerate(factor) {
-  velocityFactor.value *= factor;
-  satellite.value.vx *= factor;
-  satellite.value.vy *= factor;
-}
+function onAcelerate(factor) {}
 
 function onReset() {
-  resetSatellite();
-  tick.value = 0;
+  ticks.value = 0;
+  nodes.value = parseBodies(solarSystem);
+  forceSim.nodes(nodes.value);
 }
 
 const currentVelocity = computed(() => {
-  return Math.sqrt(
-    Math.pow(satellite.value.vx, 2) + Math.pow(satellite.value.vy, 2)
-  ).toFixed(2);
+  // return Math.sqrt(
+  //   Math.pow(satellite.value.vx, 2) + Math.pow(satellite.value.vy, 2)
+  // ).toFixed(2);
 });
 </script>
 
 <template>
   <div class="p-2 space-x-2 flex items-center">
     <button class="btn btn-primary" @click="onPause">Play/Pause</button>
-    <button class="btn btn-primary" @click="onBack"><</button>
-    <button class="btn btn-primary" @click="onForward">></button>
+    <!-- <button class="btn btn-primary" @click="onBack"><</button>
+    <button class="btn btn-primary" @click="onForward">></button> -->
     <button class="btn btn-primary" @click="onReset">Reset</button>
-    <button class="btn btn-primary" @click="onAcelerate(0.99)"><<</button>
-    <button class="btn btn-primary" @click="onAcelerate(1.01)">>></button>
-    <!-- <div class="inline-block">
-      <input
-        @change="onVelocityChange"
-        type="range"
-        min="0"
-        max="2"
-        value="1"
-        class="range range-primary"
-        step="0.25"
-      />
-    </div> -->
+    <!-- <button class="btn btn-primary" @click="onAcelerate(0.99)"><<</button>
+    <button class="btn btn-primary" @click="onAcelerate(1.01)">>></button> -->
   </div>
-  <div>
-    <svg width="600" height="600">
-      <g transform="translate(300,300)">
-        <circle r="20" fill="gold"></circle>
-        <circle
-          r="10"
-          fill="green"
-          :cx="satellite.x"
-          :cy="satellite.y"
-        ></circle>
-      </g>
-    </svg>
-  </div>
-  <div>
-    <pre>{{ currentVelocity }}</pre>
-    <pre>{{ tick }}</pre>
-    <pre>{{ satellite }}</pre>
+  <div class="flex">
+    <div>
+      <svg :width="width" :height="height">
+        <g :transform="`translate(${width / 2},${height / 2})`">
+          <circle r="20" fill="gold"></circle>
+          <circle
+            r="10"
+            fill="green"
+            :cx="nodes[1].x"
+            :cy="nodes[1].y"
+          ></circle>
+          <circle r="10" fill="red" :cx="nodes[2].x" :cy="nodes[2].y"></circle>
+        </g>
+      </svg>
+    </div>
+    <div>
+      <pre>{{ ticks }}</pre>
+      <pre>{{ nodes }}</pre>
+    </div>
   </div>
 </template>
