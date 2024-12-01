b12d0f53932f2597ac80208a728d58238a510c57
Rikus Rus <rikus.rus@gmail.com>
2024-12-01 20:40:52 +0100

    sync on alignement date

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -2,24 +2,32 @@
 import { DateTime } from "luxon";
 import { ref, computed } from "vue";
 import d3ForceMagnetic from "d3-force-magnetic";
+import {
+  Body,
+  Illumination,
+  HelioVector,
+  SearchRelativeLongitude,
+} from "astronomy-engine";
 
 const paused = ref(false);
 const ticks = ref(0);
-let years = 0;
-const scalingFactor = ref(800);
 
-//Get sun/planet/satillite data
-import solarSystem from "./../data/solarSystem.json";
+const scalingFactor = ref(800);
+let date = ref(DateTime.now());
+let years = date.value.year - 1970;
 
 //Set constants/scales
-const width = 1000;
-const height = 1000;
+const width = 800;
+const height = 800;
 const maxDistance =
   d3.max(
     solarSystem.map((d) =>
       d3.max(d.satellites.map((s) => d.distance + s.distance))
     )
   ) * 1.2;
+let speedFactor = 1000000;
+let G = 6.6743e-11 * speedFactor;
+
 const au = d3
   .scaleLinear() // Astronomical unit
   .domain([0, maxDistance])
@@ -30,8 +38,28 @@ const dateScale = d3
   .domain([0, 360])
   .range([0, 1000 * 60 * 60 * 24 * 365]);
 
-const G = 0.0005;
-const pxG = G * Math.pow(1, 3);
+const msToAngle = d3
+  .scaleLinear()
+  .domain([0, 1000 * 60 * 60 * 24 * 365])
+  .range([0, 360]);
+
+const today = new Date();
+const nextAlignemntDate = SearchRelativeLongitude(Body.Mars, 0, today).date;
+const msTillnextAlignment = nextAlignemntDate - today;
+const EarthDegTillNextAlignemnt =
+  (msTillnextAlignment / (1000 * 60 * 60 * 24 * 365)) * 360;
+const MarsDegTillNextAlignment = (EarthDegTillNextAlignemnt * 365.256) / 686.98;
+
+let earthPhase = msToAngle(date.value.toMillis() % (1000 * 60 * 60 * 24 * 365));
+let marsPhase =
+  earthPhase + (EarthDegTillNextAlignemnt - MarsDegTillNextAlignment);
+
+const nextAlignment = computed(() => {
+  return SearchRelativeLongitude(Body.Mars, 0, date.value.toJSDate()).date;
+});
+
+//Get sun/planet/satillite data
+import solarSystem from "./../data/solarSystem.json";
 
 const extractSunAndSatillite = (bodies) => {
   const sunOnly = { ...bodies[0] };
@@ -45,7 +73,7 @@ const extractSunAndSatillite = (bodies) => {
       distance,
       r: 0.00001,
       mass,
-      phase,
+      phase: earthPhase,
     },
   ];
 
@@ -64,6 +92,8 @@ function parseBodies(
 ) {
   return [].concat(
     ...bodies.map((body) => {
+      if (body.name === "earth") body.phase = earthPhase;
+      if (body.name === "mars") body.phase = marsPhase;
       const ang =
         ((body.phase !== undefined ? body.phase : Math.random() * 360) *
           Math.PI) /
@@ -73,7 +103,7 @@ function parseBodies(
       const y = posOffset[1] - body.distance * Math.cos(ang);
 
       const relVelocity =
-        (body.distance ? Math.sqrt((pxG * parentMass) / body.distance) : 0) *
+        (body.distance ? Math.sqrt((G * parentMass) / body.distance) : 0) *
         (body.factorV || 1); // orbital velocity: sqrt(GM/d)
 
       const vx = velocityOffset[0] + relVelocity * Math.cos(ang);
@@ -98,6 +128,12 @@ function parseBodies(
   );
 }
 
+// function getEarthAngle() {
+//   const earth = nodes.value.find((d) => d.name === "earth");
+//   const angleRad = Math.atan2(earth.x, -earth.y);
+//   const angle = (angleRad * 180) / Math.PI + (angleRad < 0 ? 360 : 0);
+//   return angle;
+// }
 const earthAngle = computed(() => {
   const earth = nodes.value.find((d) => d.name === "earth");
   const angleRad = Math.atan2(earth.x, -earth.y);
@@ -116,7 +152,7 @@ const forceSim = d3
       .charge((node) => node.mass)
   );
 
-forceSim.nodes(nodes.value).force("gravity").strength(pxG);
+forceSim.nodes(nodes.value).force("gravity").strength(G);
 
 let previousAngle = 0;
 
@@ -129,8 +165,12 @@ forceSim.on("tick", () => {
     setItem(body.trails, [body.x, body.y], 600);
   });
 
-  if (previousAngle > earthAngle) years++;
-  previousAngle = earthAngle;
+  date.value = DateTime.fromObject({ year: years + 1970 }).plus(
+    dateScale(earthAngle.value)
+  );
+
+  if (previousAngle > earthAngle.value) years++;
+  previousAngle = earthAngle.value;
 });
 
 const forceSim2 = d3
@@ -144,7 +184,7 @@ const forceSim2 = d3
       .charge((node) => node.mass)
   );
 
-forceSim2.nodes(satillite.value).force("gravity").strength(pxG);
+forceSim2.nodes(satillite.value).force("gravity").strength(G);
 
 forceSim2.on("tick", () => {
   const bodies = forceSim2.nodes();
@@ -175,6 +215,11 @@ function onBack() {
   // forceSim.restart().tick(tick.value).stop();
 }
 
+// function onSpeedChange(e) {
+//   G = e.target.value / 100000;
+//   onReset();
+// }
+
 function onVelocityChange(e) {}
 
 function onAcelerate(factor) {}
@@ -202,22 +247,38 @@ const currentVelocity = computed(() => {
   // ).toFixed(2);
 });
 
-const actualDistance = (x, y) => {
-  return pxTom(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
-};
+const actualDistance = computed(() => {
+  const mars = nodes.value.find((node) => node.name === "mars");
+  const distance = Math.sqrt(Math.pow(mars.x, 2) + Math.pow(mars.y, 2));
+
+  return distance;
+});
 </script>
 
 <template>
-  <div class="p-2 space-x-2 flex items-center">
-    <button class="btn btn-primary" @click="onPause">Play/Pause</button>
-    <!-- <button class="btn btn-primary" @click="onBack"><</button>
-    <button class="btn btn-primary" @click="onForward">></button> -->
-    <button class="btn btn-primary" @click="onReset">Reset</button>
-    <button class="btn btn-primary" @click="onLaunch">Launch</button>
-    <!-- <button class="btn btn-primary" @click="onAcelerate(0.99)"><<</button>
-    <button class="btn btn-primary" @click="onAcelerate(1.01)">>></button> -->
-  </div>
   <div class="flex">
+    <div>
+      <div class="p-2 space-y-2 flex flex-col">
+        <button class="btn btn-primary" @click="onPause">Play/Pause</button>
+        <!-- <button class="btn btn-primary" @click="onBack"><</button>
+    <button class="btn btn-primary" @click="onForward">></button> -->
+        <button class="btn btn-primary" @click="onReset">Reset</button>
+        <button class="btn btn-primary" @click="onLaunch">Launch</button>
+        <!-- <div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value="15"
+        class="range range-primary"
+        @change="onSpeedChange"
+      />
+    </div> -->
+
+        <!-- <button class="btn btn-primary" @click="onAcelerate(0.99)"><<</button>
+    <button class="btn btn-primary" @click="onAcelerate(1.01)">>></button> -->
+      </div>
+    </div>
     <div>
       <svg :width="width" :height="height">
         <g :transform="`translate(${width / 2},${height / 2})`">
@@ -254,13 +315,9 @@ const actualDistance = (x, y) => {
       </svg>
     </div>
     <div>
-      <pre>{{ ticks }}</pre>
-      <pre>{{
-        DateTime.fromMillis(dateScale(earthAngle))
-          .plus({ years: years })
-          .toISODate()
-      }}</pre>
-      <pre>{{ earthAngle }}</pre>
+      <pre>{{ date.toFormat("yyyy-MM-dd") }}</pre>
+      <pre>{{ nextAlignment }}</pre>
+      <!-- <pre>{{ actualDistance }}</pre> -->
     </div>
   </div>
 </template>
