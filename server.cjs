7b6b7dd096d6129714014b305c4cfab19dfda387
Rikus Rus <rikus.rus@gmail.com>
2024-12-03 17:24:18 +0100

    express history

--- server.cjs
+++ server.cjs
@@ -1,7 +1,10 @@
 const express = require("express");
 const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
 const path = require("path");
+
 app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 3000;
 app.listen(port);
